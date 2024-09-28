64341c66cd290876a042d409bc9a7b3b5f07e108
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-09-28 18:43:13 -0400

    Further development and cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,75 @@ use crossterm::{
     execute,
     terminal::{Clear, ClearType},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use reqwest::blocking::get;
-use std::io::stdout;
+use std::{error::Error, io::stdout};
+use std::fmt::Formatter;
 
-#[derive(Debug, Deserialize, Default)]
-struct App {
-    user_input: String,
-}
-
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Serialize, Deserialize, Default)]
 struct CurrentArticle {
     title: String,
     description: String,
     extract: String,
 }
 
+#[derive(Debug, Default)]
+struct App {
+    current_article: CurrentArticle,
+    search_string: String,
+}
+
+impl App {
+    fn get_article(&mut self) -> Result<(), Box<dyn Error>> {
+        let text = get(format!("{URL}/{}", self.search_string))?.text()?;
+        if let Ok(article) = serde_json::from_str::<CurrentArticle>(&text) {
+            self.current_article = article;
+        }
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for App {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "
+                    Searching for: {}
+Title: {}
+------------
+Description: {}
+------------
+{}",
+            self.search_string,
+            self.current_article.title,
+            self.current_article.description,
+            self.current_article.extract
+        )
+    }
+}
+
 const URL: &str = "https://en.wikipedia.org/api/rest_v1/page/summary";
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut app = App::default();
 
     loop {
-        if let Event::Key(key_event) = read().unwrap() {
+        println!("{app}");
+        if let Event::Key(key_event) = read()? {
             if key_event.kind == KeyEventKind::Press {
-                execute!(stdout(), Clear(ClearType::All)).unwrap();
                 match key_event.code {
                     KeyCode::Backspace => {
-                        app.user_input.pop();
-                        println!("{}", app.user_input);
+                        app.search_string.pop();
                     }
-                    KeyCode::Esc => app.user_input.clear(),
-                    KeyCode::Enter => {
-                        print!("Searching Wikipedia...");
-                        let req = get(format!("{URL}/{}", app.user_input)).unwrap();
-                        let text = req.text().unwrap();
-                        let as_article: CurrentArticle = serde_json::from_str(&text).unwrap();
-                        println!("{as_article:#?}");
-                    }
+                    KeyCode::Esc => app.search_string.clear(),
+                    KeyCode::Enter => app.get_article()?,
                     KeyCode::Char(c) => {
-                        app.user_input.push(c);
-                        println!("{}", app.user_input);
+                        app.search_string.push(c);
                     }
                     _ => {}
                 }
             }
+            execute!(stdout(), Clear(ClearType::All))?;
         }
     }
 }
